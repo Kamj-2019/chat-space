f96947a35250ea8a0fe28fcc667ad4cafdad92f3
KamJ-2019 <jnk.tcexp.ac@gmail.com>
2019-05-21 20:48:36 +0900

    Update groups_controller.rb

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,24 @@
 class GroupsController < ApplicationController
+
+def new
+  @group = Group.new
+end
+
+def create
+  @group = Group.create(group_params) if user_signed_in?
+end
+
+def edit
+  @group = Group.find(params[:id])
+end
+
+def update
+  @group = Group.update(group_params) if user_signed_in?
+end
+
+private
+def group_params
+  params.permit()
+end
+
 end
