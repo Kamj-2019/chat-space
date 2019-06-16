53ebf76a496b0047cc4f21fc84d7c8bd7c16cf89
KamJ-2019 <jnk.tcexp.ac@gmail.com>
2019-06-16 23:55:17 +0900

    Update Message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -37,28 +37,30 @@ $(function(){
   });
 
   var reloadMessages = function() {
-    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
-    var last_message_id = $('.subheading').last().data('message-id');
-    var group_id = $('.wrapper__main__cont').data('group-id');
-    $.ajax({
-      //ルーティングで設定した通りのURLを指定
-      url: '/groups/' + group_id + '/api/messages',
-      //ルーティングで設定した通りhttpメソッドをgetに指定
-      type: 'get',
-      dataType: 'json',
-      //dataオプションでリクエストに値を含める
-      data: {id: last_message_id}
-    })
-    .done(function(messages) {
-      messages.forEach(function(message){
-        var insertHTML = buildHTML(message);
-        $('.wrapper__main__cont').append(insertHTML);
-        $('.wrapper__main__cont').animate({scrollTop: $('.wrapper__main__cont')[0].scrollHeight}, 'fast');
-      });
-    })
-    .fail(function(data){
-      alert('エラーが発生しました');
-    })
+    if (window.location.href.match(/\/groups\/\d+\/messages/)){
+      //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+      var last_message_id = $('.subheading').last().data('message-id');
+      var group_id = $('.wrapper__main__cont').data('group-id');
+      $.ajax({
+        //ルーティングで設定した通りのURLを指定
+        url: '/groups/' + group_id + '/api/messages',
+        //ルーティングで設定した通りhttpメソッドをgetに指定
+        type: 'get',
+        dataType: 'json',
+        //dataオプションでリクエストに値を含める
+        data: {id: last_message_id}
+      })
+      .done(function(messages) {
+        messages.forEach(function(message){
+          var insertHTML = buildHTML(message);
+          $('.wrapper__main__cont').append(insertHTML);
+          $('.wrapper__main__cont').animate({scrollTop: $('.wrapper__main__cont')[0].scrollHeight}, 'fast');
+        });
+      })
+      .fail(function(data){
+        alert('エラーが発生しました');
+      })
+    }
   };
   setInterval(reloadMessages, 5000);
 });
