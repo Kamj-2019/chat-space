c994af09fce2dd5b51c8634c027f191216602446
KamJ-2019 <jnk.tcexp.ac@gmail.com>
2019-06-16 13:35:34 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -3,7 +3,7 @@ $(function(){
 
     var addImage = message.image? `<img class="lower-message__image" src=${message.image}>` : "";
 
-    var html = `<div class='subheading'>
+    var html = `<div class='subheading' data-message-id=${message.message_id}>
     <h1>${ message.name }</h1>
     <p>${ message.date }</p>
     </div>
@@ -35,4 +35,30 @@ $(function(){
       $('input').prop('disabled', false);
     })
   });
+
+  var reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    var last_message_id = $('.subheading').last().data('message-id');
+    var group_id = $('.wrapper__main__cont').data('group-id');
+    $.ajax({
+      //ルーティングで設定した通りのURLを指定
+      url: '/groups/' + group_id + '/api/messages',
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      messages.forEach(function(message){
+        var insertHTML = buildHTML(message);
+        $('.wrapper__main__cont').append(insertHTML);
+        $('.wrapper__main__cont').animate({scrollTop: $('.wrapper__main__cont')[0].scrollHeight}, 'fast');
+      });
+    })
+    .fail(function() {
+      console.log('error');
+    });
+  };
+  setInterval(reloadMessages, 5000);
 });
